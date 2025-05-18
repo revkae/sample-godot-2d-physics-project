7d01afec0a35ea5c41b9f7a6135ceeadea7c5ced
revkae <87928758+revkae@users.noreply.github.com>
2025-05-18 16:05:28 +0300

    finished it

--- jump_pad.gd
+++ jump_pad.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var impulse_strength = 300;
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name != "RigidBody2D": return;
-	body.apply_central_impulse(Vector2.UP.rotated(rotation) * 400)
+	body.apply_central_impulse(Vector2.UP.rotated(rotation) * impulse_strength)
